3170840097b3f646ce24ddee976e0af51f394faf
Brenda Uemura <brendauem@gmail.com>
2023-03-21 12:00:38 -0300

    ex1_algobio

---
+++ Ex1_AlgoBio.py
@@ -0,0 +1,19 @@
+''' Ex_1 Contar nucleotídeos A T C G de uma sequência
+Brenda Uemura      RA:148177              AlgoBio - 2023.1    IB - Shida '''
+
+sequencia = input("Insira a sequência a ser analisada: \n")
+
+#CONTAGEM INDIVIDUAL DOS NUCLEOTIDOS DA SEQUENCIA
+a = sequencia.count('A')       
+c = sequencia.count('C')
+g = sequencia.count('G')
+t = sequencia.count('T')
+
+print("A:", a)
+print("C:", c)
+print("G:", g)
+print("T:", t)
+
+#SOMATORIA DOS NUCLEOTIDEOS 
+s = a+c+g+t                  
+print("Total de nucleotídeos:", s)
